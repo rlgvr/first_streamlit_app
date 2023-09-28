4ac47c889d77147af453cd8c2f90d0c4b3a89004
Rolando <82373093+rlgvr@users.noreply.github.com>
2023-09-28 12:25:38 +0100

    Update streamlit_app.py
    adding suggestions to pick up list

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 ## read data from link in pandas 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt", index_col=0)
-streamlit.dataframe(my_fruit_list)
+
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+# stream dataframe
+streamlit.dataframe(my_fruit_list)
