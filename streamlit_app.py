78789d7724cee4a290edd822ab1f96481fb66abc
Rolando <82373093+rlgvr@users.noreply.github.com>
2023-10-02 15:25:24 +0100

    enable users to send requests for any product in  streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # stream dataframe
 streamlit.dataframe(fruits_to_show)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 streamlit.header("Fruityvice Fruit Advice!")
 ## integrate URL request 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
 # streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
